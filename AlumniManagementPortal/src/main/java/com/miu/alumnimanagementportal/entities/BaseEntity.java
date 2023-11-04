d1b687f6b94125854e6b11166d3b4c8b7bc7136f
ranjan66 <ranjan66@gmail.com>
2023-11-04 18:03:17 -0500

    check

--- AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/entities/BaseEntity.java
+++ AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/entities/BaseEntity.java
@@ -1,8 +1,4 @@
 package com.miu.alumnimanagementportal.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 public class BaseEntity {
 }
