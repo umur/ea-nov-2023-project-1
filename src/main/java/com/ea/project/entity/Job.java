e787913c9e58e18d2276fd7777f600cd66f18068
dinomov <dinomov@gmail.com>
2023-11-04 09:57:04 -0500

    Update Job.java
    Added attributes

--- src/main/java/com/ea/project/entity/Job.java
+++ src/main/java/com/ea/project/entity/Job.java
@@ -1,2 +1,27 @@
-package com.ea.project.entity;public class Job {
+package com.ea.project.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.util.List;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE job SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String companyName;
+    private Boolean deleted = Boolean.FALSE;
+
+    @Embedded
+    private Address address;
 }
