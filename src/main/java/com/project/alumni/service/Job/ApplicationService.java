38933f9ccbf6cc158df873fc36eac0c69fd9715f
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-04 21:43:10 -0500

    Add Job Applications Service and Controller

---
+++ src/main/java/com/project/alumni/service/Job/ApplicationService.java
@@ -0,0 +1,7 @@
+package com.project.alumni.service.Job;
+
+import com.project.alumni.dto.Job.ApplicationDto;
+import com.project.alumni.service.CrudService;
+
+public interface ApplicationService extends CrudService<ApplicationDto> {
+}
