ba741716c925cf62f82028025001d8a72bf0975b
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-05 16:57:26 -0600

    Tried to implement AuthService/Login

--- src/main/java/com/project/alumni/repository/UserRepository.java
+++ src/main/java/com/project/alumni/repository/UserRepository.java
@@ -3,16 +3,6 @@ package com.project.alumni.repository;
 import com.project.alumni.entity.User;
 import org.springframework.data.repository.ListCrudRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends ListCrudRepository<User, Long> {
-    Optional<User> findByEmail(String email);
 
-    Optional<User> findByUsernameOrEmail(String username, String email);
-
-    Optional<User> findByUsername(String username);
-
-    Boolean existsByUsername(String username);
-
-    Boolean existsByEmail(String email);
 }
