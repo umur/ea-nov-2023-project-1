2400657764a994e96083f8ce876ebd65772d3a4a
ranjan66 <ranjan66@gmail.com>
2023-11-04 21:54:58 -0500

    delete unnecessery files

--- AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/repositories/AbstractAuditableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miu.alumnimanagementportal.repositories;
-
-import org.springframework.data.jpa.domain.AbstractAuditable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface AbstractAuditableRepository<T extends AbstractAuditable> extends JpaRepository<T, PK> {
-}
--- AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/repositories/AbstractPersistableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miu.alumnimanagementportal.repositories;
-
-import org.springframework.data.jpa.domain.AbstractPersistable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface AbstractPersistableRepository<T extends AbstractPersistable> extends JpaRepository<T, PK> {
-}
