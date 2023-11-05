32cf8a85b86634488355fef67bf0a7126dece670
zhaoyan8 <zhaoyan8@xiaomi.com>
2023-11-05 09:47:51 -0600

    change

--- src/main/java/com/ea/project/dto/AnnouncementDto.java
+++ src/main/java/com/ea/project/dto/AnnouncementDto.java
@@ -1,4 +1,9 @@
 package com.ea.project.dto;
 
+import java.time.LocalDateTime;
+
 public class AnnouncementDto {
+    private int id;
+    private String content;
+    private LocalDateTime dateTime;
 }
