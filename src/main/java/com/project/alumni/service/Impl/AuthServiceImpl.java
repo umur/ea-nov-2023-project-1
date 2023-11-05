dad4221df1169d4ada36352e610f9d849a92f8d2
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-05 15:46:24 -0600

    AuthService/Login implementation

--- src/main/java/com/project/alumni/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/project/alumni/service/Impl/AuthServiceImpl.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+
+    /**
+     * Implementing our own Login logic using DAO Authentication provider for now
+     * instead of Using Spring Authentication Login functionality.
+     * To be extended later after attending Spring Security lecture.
+     * */
     @Override
     public String login(UserLoginDto loginDto) {
         return null;
