d541cf7edd121fde8be25e5005de53f63b5a3975
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-04 22:51:21 -0500

    Completed createUser in Service impl

--- src/main/java/com/project/alumni/service/Impl/UserServiceImpl.java
+++ src/main/java/com/project/alumni/service/Impl/UserServiceImpl.java
@@ -1,4 +1,29 @@
 package com.project.alumni.service.Impl;
 
-public class UserServiceImpl {
-}
+import com.project.alumni.dto.UserMinimalDto;
+import com.project.alumni.entity.User;
+import com.project.alumni.repository.UserRepository;
+import com.project.alumni.service.UserService;
+import org.modelmapper.ModelMapper;
+
+public class UserServiceImpl implements UserService {
+    private final ModelMapper modelMapper;
+
+    private final UserRepository userRepo;
+
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepo) {
+        this.modelMapper = modelMapper;
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserMinimalDto createUser(UserMinimalDto minimalDto) {
+        // Convert Dto to Entity
+        User user = modelMapper.map(minimalDto, User.class);
+        User newUser = userRepo.save(user);
+        // Convert Entity to Dto
+        return modelMapper.map(newUser, UserMinimalDto.class);
+    }
+
+
+} // End of UserServiceImpl class
--- src/main/java/com/project/alumni/service/UserService.java
+++ src/main/java/com/project/alumni/service/UserService.java
@@ -1,4 +1,9 @@
 package com.project.alumni.service;
 
+import com.project.alumni.dto.UserMinimalDto;
+
 public interface UserService {
+    // Create user information with minimal detail and user can update later
+    // with UserFullDetails
+    public UserMinimalDto createUser(UserMinimalDto minimalDto);
 }
