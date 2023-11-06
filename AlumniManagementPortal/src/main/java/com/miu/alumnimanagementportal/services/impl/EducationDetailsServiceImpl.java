476f78f8e2c9fb5dbb4b93659eb45ae720177b98
pratima81 <engpshah@gmail.com>
2023-11-05 21:21:47 -0600

    service impl added

--- AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/services/impl/EducationDetailsServiceImpl.java
+++ AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/services/impl/EducationDetailsServiceImpl.java
@@ -4,9 +4,11 @@ import com.miu.alumnimanagementportal.common.Converter;
 import com.miu.alumnimanagementportal.dtos.AttendantDto;
 import com.miu.alumnimanagementportal.dtos.EducationDetailsDto;
 import com.miu.alumnimanagementportal.entities.Attendant;
+import com.miu.alumnimanagementportal.entities.EducationDetails;
 import com.miu.alumnimanagementportal.exceptions.DataAlreadyExistException;
 import com.miu.alumnimanagementportal.exceptions.ResourceNotFoundException;
 import com.miu.alumnimanagementportal.repositories.AttendantRepository;
+import com.miu.alumnimanagementportal.repositories.EducationDetailsRepository;
 import com.miu.alumnimanagementportal.services.EducationDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +21,7 @@ import java.util.stream.Collectors;
 @Service
 public class EducationDetailsServiceImpl implements EducationDetailsService {
 
-    private final AttendantRepository educationDetailsRepository;
+    private final EducationDetailsRepository educationDetailsRepository;
     private final Converter converter;
 
     @Override
@@ -29,7 +31,7 @@ public class EducationDetailsServiceImpl implements EducationDetailsService {
                 throw new DataAlreadyExistException("EducationDetails with id " + id + " already exists");
             }
         });
-        educationDetailsRepository.save(converter.convert(educationDetailsDto, Attendant.class));
+        educationDetailsRepository.save(converter.convert(educationDetailsDto, EducationDetails.class));
     }
 
 
@@ -46,7 +48,7 @@ public class EducationDetailsServiceImpl implements EducationDetailsService {
             if (!educationDetailsRepository.existsById(entityId)) {
                 throw new ResourceNotFoundException("EducationDetails with id " + entityId + " not found");
             }
-           return converter.convert(educationDetailsRepository.save(converter.convert(educationDetailsDto, Attendant.class)), EducationDetailsDto.class);
+           return converter.convert(educationDetailsRepository.save(converter.convert(educationDetailsDto, EducationDetails.class)), EducationDetailsDto.class);
         }).orElseThrow(() -> new ResourceNotFoundException("EducationDetails with id " + id + " not found"));
     }
 
