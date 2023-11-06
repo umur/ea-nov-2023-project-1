f37de347b4fe44bdb4afb21bfd06da43380f30c1
Mohammad Serajul Kabir Chy Rubel <135068982+mskabir2023@users.noreply.github.com>
2023-11-06 14:17:01 -0600

    Survey Controller

---
+++ AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/controllers/SurveyController.java
@@ -0,0 +1,55 @@
+package com.miu.alumnimanagementportal.controllers;
+
+import com.miu.alumnimanagementportal.common.Converter;
+import com.miu.alumnimanagementportal.dtos.JobPostDto;
+import com.miu.alumnimanagementportal.dtos.ProfileDto;
+import com.miu.alumnimanagementportal.dtos.SurveyDto;
+import com.miu.alumnimanagementportal.dtos.UserDto;
+import com.miu.alumnimanagementportal.services.ProfileService;
+import com.miu.alumnimanagementportal.services.SurveyService;
+import com.miu.alumnimanagementportal.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/surveys")
+public class SurveyController {
+    private final SurveyService surveyService;
+    private final Converter converter;
+
+    @PostMapping
+    public ResponseEntity<?> createSurvey(@Valid @RequestBody SurveyDto surveyDto) {
+        surveyService.create(surveyDto);
+        return converter.buildReposeEntity(Map.of("message", "Survey created successfully"), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getSurveyAll() {
+        return converter.buildReposeEntity(Map.of("data", surveyService.findAll()), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSurveyById(@PathVariable Long id) {
+        return converter.buildReposeEntity(Map.of("data", surveyService.getSurveyById(id)), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateSurveyById(@Valid @PathVariable Long id, @RequestBody SurveyDto surveyDto) {
+        return converter.buildReposeEntity(Map.of("data", surveyService.update(surveyDto, id)), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteSurveyById(@PathVariable Long id) {
+        surveyService.delete(id);
+        return converter.buildReposeEntity(Map.of("message", "Survey Deleted successfully"), HttpStatus.ACCEPTED);
+    }
+
+
+}
