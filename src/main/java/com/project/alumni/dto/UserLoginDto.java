eda3075d80ad76c12253351b4d728118b497cf34
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-05 19:14:29 -0600

    Tried to implement AuthService/Login/register

--- src/main/java/com/project/alumni/dto/UserLoginDto.java
+++ src/main/java/com/project/alumni/dto/UserLoginDto.java
@@ -1,6 +1,5 @@
 package com.project.alumni.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
