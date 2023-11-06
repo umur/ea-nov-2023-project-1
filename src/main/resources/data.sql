3ba3dc940d32483e0b6d649b8a0cfd39fa3192bf
MOAID <moaidmoaidrazhy@gmail.com>
2023-11-05 18:16:24 -0600

    Chat Feature Dummy Data Added to the initializer

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -246,3 +246,67 @@ VALUES (0, '2023-11-01', 1, 1, 1),
     (1, '2023-11-09', 9, 9, 9),
     (0, '2023-11-10', 10, 10, 10);
 -- Job Postings --
+
+-- chats
+INSERT INTO chats (id, is_group, name, created_at)
+values (1, false, 'one to one 1', '2023-11-01'),
+       (2, false, 'one to one 2', '2023-11-02'),
+       (3, false, 'one to one 3', '2023-11-03'),
+       (4, false, 'one to one 4', '2023-11-04'),
+       (5, false, 'one to one 5', '2023-11-05'),
+       (6, true, 'group1', '2023-11-06'),
+       (7, true, 'group2', '2023-11-07'),
+       (8, true, 'group3', '2023-11-08'),
+       (9, true, 'group4', '2023-11-09'),
+       (10, true, 'group5', '2023-11-10');
+
+--  chat_participants
+INSERT INTO chats_participants (chat_id, participants_id)
+values (1, 1),
+       (1, 2),
+       (2, 1),
+       (2, 3),
+       (3, 1),
+       (3, 4),
+       (4, 1),
+       (4, 5),
+       (5, 1),
+       (5, 6),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (7, 1),
+       (7, 2),
+       (7, 3),
+       (8, 1),
+       (8, 2),
+       (8, 3),
+       (9, 1),
+       (9, 2),
+       (9, 3),
+       (10, 1),
+       (10, 2),
+       (10, 3);
+
+-- messages
+INSERT INTO chat_messages (id, chat_id, sender_id, type, content, created_at)
+values (1, 1, 1, 'text', 'hello', '2023-11-01'),
+       (2, 1, 2, 'text', 'hi', '2023-11-02'),
+       (3, 1, 1, 'text', 'how are you?', '2023-11-03'),
+       (4, 1, 2, 'text', 'I am fine', '2023-11-04'),
+       (5, 1, 1, 'text', 'what are you doing?', '2023-11-05'),
+       (6, 1, 2, 'text', 'nothing much', '2023-11-06'),
+       (7, 1, 1, 'text', 'ok', '2023-11-07'),
+       (8, 1, 2, 'text', 'bye', '2023-11-08'),
+       (9, 2, 1, 'text', 'hello', '2023-11-09'),
+       (10, 2, 3, 'text', 'hi', '2023-11-10'),
+       (11, 2, 1, 'text', 'how are you?', '2023-11-11'),
+       (12, 2, 3, 'text', 'I am fine', '2023-11-12'),
+       (13, 2, 1, 'text', 'what are you doing?', '2023-11-13'),
+       (14, 2, 3, 'text', 'nothing much', '2023-11-14'),
+       (15, 2, 1, 'text', 'ok', '2023-11-15'),
+       (16, 2, 3, 'text', 'bye', '2023-11-16'),
+       (17, 3, 1, 'text', 'hello', '2023-11-17'),
+       (18, 3, 4, 'text', 'hi', '2023-11-18'),
+       (19, 3, 1, 'text', 'how are you?', '2023-11-19'),
+       (20, 3, 4, 'text', 'I am fine', '2023-11-20');
