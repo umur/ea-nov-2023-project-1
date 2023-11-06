39ef2740a886eda0773b55d110b1f63bf99157d9
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-05 19:45:00 -0600

    Tried to implement AuthService/Login/register

--- src/main/java/com/project/alumni/service/AuthService.java
+++ src/main/java/com/project/alumni/service/AuthService.java
@@ -6,5 +6,5 @@ import com.project.alumni.dto.UserMinimalDto;
 public interface AuthService {
     public String login(UserLoginDto loginDto);
 
-    String register(UserMinimalDto registerDto);
+    public String register(UserMinimalDto registerDto);
 }
