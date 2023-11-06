12b5aa72f137594dcad5ddef0f3359a8c633a460
pratima81 <engpshah@gmail.com>
2023-11-06 15:52:43 -0600

    resolved merge conflict

--- AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/services/impl/UserServiceImpl.java
+++ AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/services/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.miu.alumnimanagementportal.services.impl;
 
 import com.miu.alumnimanagementportal.common.Converter;
+import com.miu.alumnimanagementportal.dtos.UserActivationDto;
 import com.miu.alumnimanagementportal.dtos.UserDto;
+import com.miu.alumnimanagementportal.dtos.UserLoginInfoDto;
 import com.miu.alumnimanagementportal.exceptions.DataAlreadyExistException;
 import com.miu.alumnimanagementportal.exceptions.ResourceNotFoundException;
 import com.miu.alumnimanagementportal.repositories.UserRepository;
@@ -10,6 +12,9 @@ import com.miu.alumnimanagementportal.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 import java.util.Optional;
 
@@ -51,6 +56,8 @@ public class UserServiceImpl implements UserService {
         }).orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
     }
 
+
+
     @Override
     public UserDto getUserById(Long id) {
         return Optional.ofNullable(id)
@@ -63,4 +70,48 @@ public class UserServiceImpl implements UserService {
     public void delete(Long id) {
         Optional.ofNullable(id).ifPresent(repository::deleteById);
     }
+
+
+
+    @Override
+    public UserActivationDto update(UserActivationDto userActivationDto, Long id) {
+        return Optional.ofNullable(userActivationDto.getId()).map(entityId -> {
+            if (!repository.existsById(entityId)) {
+                throw new ResourceNotFoundException("User with id " + entityId + " not found");
+            }
+            return converter.convert(
+                    repository.save(
+                            converter.convert(userActivationDto, User.class)
+                    ), UserActivationDto.class);
+        }).orElseThrow(() -> new ResourceNotFoundException("User  not found"));
+    }
+
+    private static final int MAX_LOGIN_ATTEMPTS = 5;
+    // 15 minutes in milliseconds
+
+    @Override
+    public void login(UserLoginInfoDto userLoginInfoDto) {
+        Optional.ofNullable(userLoginInfoDto.getEmail())
+                .map(repository::findByEmail)
+                .ifPresent(user -> {
+                    if (user.is_locked()) {
+                        throw new ResourceNotFoundException("User is locked");
+                    }
+                    if (user.getLoginCount() >= MAX_LOGIN_ATTEMPTS) {
+                        throw new ResourceNotFoundException("User reached max login attempts");
+                    }
+                    LocalDateTime lastLockedDateTime = LocalDateTime.now();
+                    Duration duration = Duration.between(lastLockedDateTime, LocalDateTime.now());
+                    if (duration.toMinutes() > 15) {
+                        throw new ResourceNotFoundException("User is locked");
+                    }
+                    if (!user.getPassword().equals(userLoginInfoDto.getPassword())) {
+                        user.setLoginCount(user.getLoginCount() + 1);
+                        throw new ResourceNotFoundException("Incorrect password");
+                    }
+                    user.setLoginCount(0);
+                });
+    }
+
+
 }
