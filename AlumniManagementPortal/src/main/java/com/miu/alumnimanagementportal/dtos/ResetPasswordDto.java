f077dc1cd29bf5dd5fdb4633c5cfc9798b5f06fd
Mohammad Serajul Kabir Chy Rubel <135068982+mskabir2023@users.noreply.github.com>
2023-11-06 18:16:02 -0600

    resetPassword

---
+++ AlumniManagementPortal/src/main/java/com/miu/alumnimanagementportal/dtos/ResetPasswordDto.java
@@ -0,0 +1,13 @@
+package com.miu.alumnimanagementportal.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordDto {
+    private String email;
+    private String password;
+}
