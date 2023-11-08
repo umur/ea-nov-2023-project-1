9d42c355c197e4959432d7967537331fbd67803a
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-08 09:37:29 -0600

    Sping Data Custom Query Method for searching users

--- src/main/java/com/project/alumni/repository/UserRepository.java
+++ src/main/java/com/project/alumni/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.project.alumni.repository;
 
 import com.project.alumni.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Collection;
@@ -9,7 +10,15 @@ import java.util.List;
 public interface UserRepository extends ListCrudRepository<User, Long> {
     List<User> findAllByIdIn(List<Long> userIds);
 
+    @Query("SELECT u FROM User u WHERE " +
+    "u.industry LIKE CONCAT ('%', :query, '%')" +
+    "Or u.gradYear LIKE CONCAT ('%', :query, '%')" +
+    "Or u.address.city LIKE CONCAT ('%', :query, '%')" +
+    "Or u.address.state LIKE CONCAT ('%', :query, '%')" )
+    //"Or u.courses LIKE CONCAT ('%', :query, '%')")
+
     List<User> findByAddressId(Long addressId);
+
 
     List<User> findAllByGradYearContainsOrCoursesIdInOrAddressCityContainsOrAddressStateContainsOrIndustryContains(String gradYear, Collection<Long> courses_id, String address_city, String address_state, String industry);
 }
