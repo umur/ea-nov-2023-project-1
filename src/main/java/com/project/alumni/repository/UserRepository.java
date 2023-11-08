6b365846403870057116dd0ce06d446982001736
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-08 16:36:14 -0600

    Completed JPA custom query to Search users directory

--- src/main/java/com/project/alumni/repository/UserRepository.java
+++ src/main/java/com/project/alumni/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.project.alumni.repository;
 import com.project.alumni.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends ListCrudRepository<User, Long> {
     List<User> findAllByIdIn(List<Long> userIds);
 
     @Query("SELECT u FROM User u left join u.courses courses WHERE " +
@@ -23,7 +24,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u left join u.courses courses where " +
     "u.gradYear like concat('%', ?1, '%')" +
-    "or courses.name in ?2 or u.address.city like concat('%', ?3, '%')" +
+    "or courses.id in ?2" +
+            "or u.address.city like concat('%', ?3, '%')" +
     "or u.address.state like concat('%', ?4, '%')" +
             "or u.industry like concat('%', ?5, '%')")
     List<User> findUsersInDirectory(String gradYear,
