e4b25252ccabfbb7bfa8390f0bfec20dd47be0c1
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-08 17:00:18 -0600

    Completed JPA custom query to Search users directory

--- src/main/java/com/project/alumni/repository/UserRepository.java
+++ src/main/java/com/project/alumni/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.project.alumni.repository;
 
 import com.project.alumni.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
