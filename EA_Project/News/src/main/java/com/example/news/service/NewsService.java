c52d1212b6c0b9a4b32db7efd6448c4b943c5916
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-11-19 16:29:13 -0600

    News

--- EA_Project/News/src/main/java/com/example/news/service/NewsService.java
+++ EA_Project/News/src/main/java/com/example/news/service/NewsService.java
@@ -2,17 +2,17 @@ package com.example.news.service;
 
 
 import com.example.news.model.News;
-import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
-@Service
-public interface NewsService {
-    public void add(News news);
-    public void remove(int id);
-    public List<News> findAll();
-    public void update(News news);
 
-    public News get(int id);
+public interface NewsService {
+     void add(News news);
+     void remove(int id);
+     List<News> findAll();
+     void update(News news);
+
+     News get(int id);
 
 }
