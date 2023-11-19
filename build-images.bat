c693190823ebe68a81d656e629875ad17660b695
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-19 16:26:00 -0600

    Fix docker connections and add dynamic IP addresses

---
+++ build-images.bat
@@ -0,0 +1,6 @@
+docker build ./ServiceDiscovery -t servicediscovery
+docker build ./APIGatewayService -t apigateway
+docker build ./ConfigurationService -t configserver
+docker build ./EventService -t eventservice
+
+
