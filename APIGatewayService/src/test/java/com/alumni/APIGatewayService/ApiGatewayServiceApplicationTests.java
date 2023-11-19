2b3426b7f2309a3286d5f9fb59c1ce5555d2276d
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-19 15:17:02 -0600

    Add APIGatewayService

---
+++ APIGatewayService/src/test/java/com/alumni/APIGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumni.APIGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
