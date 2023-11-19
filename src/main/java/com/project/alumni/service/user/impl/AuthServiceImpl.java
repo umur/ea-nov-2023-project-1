0ddc3aca961c7d06843042ed1eccbf3bae0520d7
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-18 22:52:21 -0600

    Completed user autherntication service

--- src/main/java/com/project/alumni/service/user/impl/AuthServiceImpl.java
+++ src/main/java/com/project/alumni/service/user/impl/AuthServiceImpl.java
@@ -2,13 +2,23 @@ package com.project.alumni.service.user.impl;
 
 import com.project.alumni.dto.user.LoginDto;
 import com.project.alumni.dto.user.UserMinimalDto;
+import com.project.alumni.entity.user.Role;
+import com.project.alumni.entity.user.User;
+import com.project.alumni.exceptions.GlobalAPIException;
+import com.project.alumni.repository.user.RoleRepository;
+import com.project.alumni.repository.user.UserRepository;
 import com.project.alumni.security.JwtTokenProvider;
 import com.project.alumni.service.user.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -19,10 +29,21 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           JwtTokenProvider jwtTokenProvider,
+                           UserRepository userRepo, RoleRepository roleRepo,
+                           PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -39,6 +60,29 @@ public class AuthServiceImpl implements AuthService {
     }
     @Override
     public String register(UserMinimalDto registerDto) {
-        return null;
+        // Checking for existing username already exists in the database
+        if(userRepo.existsByUsername(registerDto.getUsername())){
+            throw new GlobalAPIException(HttpStatus.BAD_REQUEST, "Username already exists!");
+        }
+        // Checking for email already exists in the database
+        if(userRepo.existsByEmail(registerDto.getEmail())){
+            throw new GlobalAPIException(HttpStatus.BAD_REQUEST, "Email already exists!");
+        }
+
+        User user = new User();
+        user.setFirstName(registerDto.getFirstName());
+        user.setLastName(registerDto.getLastName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        // Whenever, user registers, we assign a default role ROLE_USER
+        Role userRole = roleRepo.findByName("ROLE_USER").orElseThrow(RuntimeException::new);
+        roles.add(userRole);
+        user.setRoles(roles);
+        userRepo.save(user);
+
+        return "User registered successfully!";
     }
 }
