b3b5beae59b041f6aac1877c4944cc618becf271
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-20 07:29:30 -0600

    Added User-Service

---
+++ course-service/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+-- Courses -- id and name
+
+INSERT INTO courses (id, name)
+VALUES (1, 'Software Engineering'),
+       (2, 'Marketing'),
+       (3, 'Accounting'),
+       (4, 'IT Support'),
+       (5, 'Graphic Design'),
+       (6, 'Business Administration'),
+       (7, 'Statistics'),
+       (8, 'Computer Science'),
+       (9, 'Networking'),
+       (10, 'Sales');
+
+-- Courses -- id and name
