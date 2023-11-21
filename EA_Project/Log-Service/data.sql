1eceb3a10757bb2aff506573873530123845ef8c
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-11-21 13:06:24 -0600

    Add files via upload

---
+++ EA_Project/Log-Service/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO log (time, description, user_id) VALUES
+    ('2023-01-01 08:00:00', 'User login successful', 1),
+    ('2023-01-02 12:30:00', 'User profile updated', 2),
+    ('2023-01-03 15:45:00', 'Password reset request', 3),
+    ('2023-01-04 10:20:00', 'New alumni registration', 4),
+    ('2023-01-05 14:00:00', 'System maintenance started', 5);
