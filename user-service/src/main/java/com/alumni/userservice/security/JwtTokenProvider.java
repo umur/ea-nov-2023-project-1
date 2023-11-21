760aaf2db44c0d3f7498c181cd12ee4df119d4fa
dunky-star <dunkygeoffrey39@gmail.com>
2023-11-20 23:56:14 -0600

    Completed implementing user-service

--- user-service/src/main/java/com/alumni/userservice/security/JwtTokenProvider.java
+++ user-service/src/main/java/com/alumni/userservice/security/JwtTokenProvider.java
@@ -1,6 +1,6 @@
 package com.alumni.userservice.security;
 
-import com.project.alumni.exceptions.GlobalAPIException;
+import com.alumni.userservice.exceptions.GlobalAPIException;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
