aeb359635351225e74f1b08e474dc667d5f923c9
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-21 15:24:42 -0600

    fix merge issues

--- build-images.bat
+++ build-images.bat
@@ -1,9 +1,7 @@
 docker build ./service-discovery -t servicediscovery
 docker build ./api-gateway -t apigateway
-docker build ./configuration-service -t configserver
-docker build ./EventService -t eventservice
-docker build ./JobService -t jobservice
 docker build ./config-server -t configserver
 docker build ./EventService -t eventservice
+docker build ./JobService -t jobservice
 docker build ./user-service -t userservice
 docker build ./chat-service -t chatservice
