8d47df2127e2b9291110f3b01b08d7c42071430b
dinomov <dinomov@gmail.com>
2023-11-22 11:07:39 -0600

    refactor

--- user-service/src/main/java/com/ea/project/service/impl/MessageServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.ea.project.service.impl;
-
-import com.ea.project.service.MessageService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageServiceImpl implements MessageService {
-}
